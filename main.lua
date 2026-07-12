c882f8ead14b17b2269674b8fcb95af11076b547
Maximoucce <rivet.maxime6@gmail.com>
2026-07-12 15:40:52 +0200

    remove font

--- main.lua
+++ main.lua
@@ -5,16 +5,6 @@ SMODS.Atlas {
     py = 34
 }
 
-SMODS.Font{
-    key = "egy-hier",
-    path = "ancient-egyptian-hieroglyphs.ttf",
-    render_scale = 500,
-    TEXT_HEIGHT_SCALE = 0.83,
-    TEXT_OFFSET = {x=0,y=0},
-    FONTSCALE = 0.1,
-    DESCSCALE = 1
-}
-
 local consumables_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/consumables")
 for _, file in ipairs(consumables_src) do
     assert(SMODS.load_file("src/consumables/" .. file))()
